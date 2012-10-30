b01d4932b95e68a56dc6d6bd9e49fe6f78adf538
marwi509 <marwi509@student.liu.se>
2012-10-30 15:18:14 +0100

    Added an empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zchat::Application.config.secret_token = '97d037f82cb849f23e9db870b7fb1e1cd0b56e094e9aebaa2db795e562d090ce4df1b11aec57b670ac2a1bf6d44d8ec047c98b6206cd21b7da5012485e8d8068'
